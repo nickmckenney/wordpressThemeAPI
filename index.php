2a9cdaf721dbf4256bcd7af4456dd7f170b2bd6b
nickmckenney <nick.mckenney@fool.com>
2020-07-11 16:01:04 -0400

    API working

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-HI!
+<?php
+$results=wp_remote_retrieve_body( wp_remote_get('https://api.nasa.gov/planetary/apod?api_key=GJ6Jvh1c8erMgH5edhmmQ538fNHcSbldUeAc68AK') );
+echo '<pre>';
+print_r($results);
+echo '</pre>';
+
